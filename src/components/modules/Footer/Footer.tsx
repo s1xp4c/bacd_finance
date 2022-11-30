e5860c7f759baa607521c94fa6b9b96d01d01cad
s1xp4c <blockstarterinfo@gmail.com>
2022-11-30 16:48:42 +0100

    Footer styling

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -1,26 +1,27 @@
 import { Box, Link, Text } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem, useColorModeValue } from '@chakra-ui/react';
 
 const links = {
   github: 'https://github.com/s1xp4c/bacd_finance',
 };
 
 const Footer = () => {
+  const hoverGrColor = useColorModeValue('gray.100', 'gray.700');
   return (
-    <Box textAlign={'center'} w="full" p={6}>
+    <Box textAlign={'center'} w="50%" p={6}>
       <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-        <GridItem w="100%" h="10" bg="blue.500">
+        <GridItem border="2px" borderColor={hoverGrColor} borderRadius="xl" padding="24px 18px">
           <Text>
             <Link href={links.github} isExternal alignItems={'center'}>
               github <ExternalLinkIcon />
             </Link>
           </Text>
         </GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
-        <GridItem w="100%" h="10" bg="blue.500"></GridItem>
+        <GridItem></GridItem>
+        <GridItem></GridItem>
+        <GridItem></GridItem>
+        <GridItem></GridItem>
       </Grid>
     </Box>
   );
