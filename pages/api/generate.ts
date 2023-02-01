2b20e67b4c891d7f0db227ad45814313e2f4cfd7
s1xp4c <blockstarterinfo@gmail.com>
2023-02-01 10:43:55 +0100

    generateJS script converted to Typescript Next-auth for online recognition

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,11 +1,12 @@
 import { Configuration, OpenAIApi } from 'openai';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async function (req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (!configuration.apiKey) {
     res.status(500).json({
       error: {
@@ -34,25 +35,16 @@ export default async function (req, res) {
     });
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        },
-      });
-    }
+    res.status(500).json({ error });
+    console.error(error);
   }
 }
 
-function generatePrompt(coin) {
-  const capitalizedCoin = coin[0].toUpperCase() + coin.slice(1).toLowerCase();
+function generatePrompt(coin: string | string[]) {
+  const capitalizedCoin = coin[0].toUpperCase() + coin.slice(1);
+  const capCoinToLower = capitalizedCoin.toLowerCase();
   return `List 5 similar crypto coins by on chain metrics from top 300 by marketcap with in depth explainations on why.
 
-Coin: ${capitalizedCoin}
+Coin: ${capCoinToLower}
 Names:`;
 }
