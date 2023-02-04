7992a45257bed11d7e93d800238478a39e93c6db
s1xp4c <blockstarterinfo@gmail.com>
2023-02-04 13:39:08 +0100

    Fixed Head loop

--- src/components/layouts/Default/Default.tsx
+++ src/components/layouts/Default/Default.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from 'react';
-import { Container, Link } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 import { Footer, Header } from 'components/modules';
 import Head from 'next/head';
 
@@ -8,11 +8,11 @@ const Default: FC<{ children: ReactNode; pageName: string }> = ({ children, page
     <Head>
       <title>{`${pageName} | BACD FINANCE`}</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <Link rel="apple-touch-icon" size="180x180" href="/apple-touch-icon.png" />
-      <Link rel="icon" type="image/png" size="32x32" href="/favicon-32x32.png" />
-      <Link rel="icon" type="image/png" size="16x16" href="/favicon-16x16.png" />
-      <Link rel="manifest" href="/site.webmanifest" />
-      <Link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#603cba" />
     </Head>
     <Header />
